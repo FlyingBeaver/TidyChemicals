8be8f767c35e3326c50b6fd46dc6000e0aca0447
Sasha <aliaqsandr@gmail.com>
2023-07-09 01:59:10 +0300

    Styling for DateInput has been added.

--- under_development/front_end/tryingVUEonForm/form-vue/src/components/DateInput.vue
+++ under_development/front_end/tryingVUEonForm/form-vue/src/components/DateInput.vue
@@ -1,16 +1,17 @@
 <template>
     <input type="date" v-bind:name="inputName" v-bind:disabled="disabled" class="simple_text_value">
-    <p>
+    <label class="date-single-checkbox">
         <input
             type="checkbox"
             v-bind:name="inputName.concat('_include')"
-            v-bind:id="inputName.concat('_include')"
             class="checkbox"
         >
-        <label
-            v-bind:for="inputName.concat('_include')"
-        >include date above in the range</label>
-    </p>
+        <div class="pretty-checkbox">
+            <div class="indicator">
+            </div>
+        </div>
+        <div class="checkbox-text">include date above in the range</div>
+    </label>
 </template>
 
 <script>
@@ -21,7 +22,52 @@ export default {
 </script>
 
 <style>
+    label.date-single-checkbox {
+        display: grid;
+        grid-template-columns: 0 30px 1fr;
+        grid-template-rows: 30px;
+        margin-top: 10px;
+    }
     input.checkbox {
-        margin: 10px;
+        border: 0;
+        width: 0;
+        height: 0;
+        opacity: 0;
+        position: static;
+    }
+    div.checkbox-text {
+        color: cornflowerblue;
+        margin: auto;
+    }
+    /* unchecked */
+    div.pretty-checkbox {
+        width: 30px;
+        height: 30px;
+        border: solid cornflowerblue 3px;
+        box-sizing: border-box;
+    }
+    div.indicator {
+        width: 0;
+        height: 0;
+        opacity: 0;
+    }
+    /* under cursor */
+    div.pretty-checkbox:hover {
+        background-color: white;
+    }
+    /* checked */
+    input.checkbox:checked ~ div.pretty-checkbox {
+        background: cornflowerblue;
+    }
+    input.checkbox:checked ~ div.pretty-checkbox > div.indicator {
+        position: relative;
+        opacity: 100%;
+        left: 9px;
+        top: 5px;
+        width: 5px;
+        height: 10px;
+        border: solid white;
+        border-width: 0 3px 3px 0;
+        transform: rotate(45deg);
     }
 </style>
